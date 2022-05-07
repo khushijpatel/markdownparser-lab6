f0b90cd1f23c96d17a236b54491e7b96e4dc0366
Khushi J Patel <cs15lsp22afa@ieng6-203.ucsd.edu>
2022-05-07 10:04:49 -0700

    fix merge issue after pull

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -27,3 +27,32 @@ public class MarkdownParseTest {
         */
     }
 }
+/*=======
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+public class MarkdownParseTest {
+    
+    
+    @Test
+    public void getLinksTest() throws IOException {
+        Path fileName = Path.of("test1.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+        ArrayList<String> expected = new ArrayList<>();
+        expected.add("https://something.com");
+        assertEquals(expected, links);
+
+        Path fileName2 = Path.of("lab5_test.md");
+        String content2 = Files.readString(fileName2);
+        ArrayList<String> links2 = MarkdownParse.getLinks(content2);
+        ArrayList<String> expected2 = new ArrayList<>();
+        expected2.add("www.google.com");
+        assertEquals(expected2, links2);
+    }
+}
+>>>>>>> a45b7dd (commiting a println for content)*/
